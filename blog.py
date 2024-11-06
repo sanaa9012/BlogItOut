6fa5f20b1157ce41b65f0ae70c1c271bda586e6e
Sana Akhunji <113094148+sanaa9012@users.noreply.github.com>
2024-11-06 19:24:51 +0530

    changed

--- blog.py
+++ blog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0dc581f85e5eeab7328bd4f3bfe37ad4'
 
 posts = [
     {
@@ -17,22 +20,27 @@ posts = [
     }
 ]
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html' , posts=posts)
 
-@app.route('/chatbot')
-def chat():
-    return render_template('chatbot.html')    
-
-@app.route('/health_camp')
-def health():
-    return render_template('health_camp.html')    
-
-@app.route('/patient_info')
+@app.route("/about")
 def patient():
-    return render_template('patient_info.html')    
+    return render_template('about.html', title='About')    
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for { form.username.data }!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='LogIn', form=form)    
+    
 if __name__ == '__main__':
     app.run(debug=True)
