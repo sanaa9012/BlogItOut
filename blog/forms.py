65d89ac4da6b2fcfec2334ca4f55277af0a6a635
Sana Akhunji <113094148+sanaa9012@users.noreply.github.com>
2024-12-17 15:13:32 +0530

    up

--- blog/forms.py
+++ blog/forms.py
@@ -15,7 +15,6 @@ class RegistrationForm(FlaskForm):
     
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log In')
